6d332b51fcfe693a7289a01ae6d8754a34d8d9d9
Peter Hahn <peterkunho@gmail.com>
2018-05-24 23:51:28 -0400

    adjusted some pages

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,30 +1,56 @@
 import React, { Component } from 'react';
 
+
+
 class Header extends Component {
+    constructor(props){
+        super(props)
+
+    }
+    active(link) {
+        return link === this.props.active
+    }
+
     render() {
+
+
         return(
             <div className="header">
                 <div>Peter Hahn</div>
                 <div className="buttons">
-                    <HeaderButton text={"Home"}/>
-                    <HeaderButton text={"Experience"}/>
-                    <HeaderButton text={"University Research"}/>
-                    <HeaderButton text={"Independent Projects"}/>
-                    <HeaderButton text={"Other"}/>
+                    <HeaderButton text={"Home"} link="/" active={this.active("/")}/>
+                    <HeaderButton text={"Experience"} link="/experience" active={this.active("/experience")}/>
+                    <HeaderButton text={"University Research"} link="/research" active={this.active("/research")}/>
+                    <HeaderButton text={"Independent Projects"} link="projects" active={this.active("/projects")}/>
+                    <HeaderButton text={"Other"} link="/other" active={this.active("/other")}/>
                 </div>
             </div>
         )
     }
 }
 
+const activeStyle = {
+    backgroundColor: `#666666`
+}
+const inactiveStyle = {
+    
+}
+
 class HeaderButton extends Component {
     render() {
+
+        const style = this.props.active ? (
+            activeStyle
+        ) : inactiveStyle
         return(
-            <div className="header-button">
-                <div className="header-button-inner">
-                    {this.props.text}
+            <a className="header-link" href={this.props.link}>
+                <div className="header-button" style={style}>
+                    <div className="header-button-inner">
+                        {this.props.text}
+                    </div>
                 </div>
-            </div>
+
+            </a>
         )
     }
 }
--- src/Main.jsx
+++ src/Main.jsx
@@ -9,32 +9,82 @@ class Main extends Component {
     render() {
         return(
             <div>
-                <Header/>
-                <Body/>
-            </div>
-        )
-    }
-}
-
-
-
-class Body extends Component {
-    render() {
-        return(
-            <div className="body">
                 <Switch>
-                    <Route exact path="/" component={TempOne}/>
-                    <Route exact path="/hey" component={TempTwo}/>
+                    <Route exact path="/" component={HomeContent}/>
+                    <Route exact path="/experience" component={ExperienceContent}/>
+                    <Route exact path="/research" component={ResearchContent}/>
+                    <Route exact path="/projects" component={ProjectContent}/>
+                    <Route exact path="/other" component={OtherContent}/>
+
                 </Switch>
             </div>
         )
     }
 }
 
-class TempOne extends Component {
+
+
+
+
+
+class HomeContent extends Component {
+
     render() {
         return(
-            <div>TempOne</div>
+            <div>
+                <Header active="/"/>
+
+                <div className='text-block'>
+                    Peter Hahn, Computer Science BS at Brown University, 
+                    specializing in Artificial Intelligence 
+                    and Software Engineering. During my time at Brown, I have 
+                    participated in a series of software projects, both independent
+                    and organized, as well as working with several research groups. 
+                    In addition, I also help out our school community's Computer 
+                    Science Department as a Consultant as well as a Teaching Assistant. 
+
+                </div>
+            </div>
+        )
+    }
+}
+
+class ExperienceContent extends Component {
+    render() {
+        return(
+            <div>                
+                <Header active="/experience"/>
+            </div>
+        )
+    }
+}
+
+class ResearchContent extends Component {
+    render() {
+        return(
+            <div>                
+                <Header active="/research"/>
+            </div>
+        )
+    }
+}
+
+class ProjectContent extends Component {
+    render() {
+        return(
+            <div>                
+                <Header active="/projects"/>
+            </div>
+        )
+    }
+}
+
+class OtherContent extends Component {
+    render() {
+        return(
+            <div>                
+                <Header active="/other"/>
+            </div>
         )
     }
 }
